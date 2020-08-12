541f68b50e788cb5c5f7d9892b8059fbab8c4230
tinca smith alistari <nicoletaalistari@macbook-pro-2.home>
2020-08-12 04:28:46 +0100

    removed interfacing

--- Viridithas.py
+++ Viridithas.py
@@ -14,41 +14,6 @@ from operator import itemgetter
 #from flask import url_for
 #from dotenv import load_dotenv
 #from authlib.integrations.flask_client import OAuth
-if True:
-    '''
-    app = Flask(__name__)
-    app.secret_key = os.getenv("SECRET_KEY")
-    print(app.secret_key)
-    app.config['LICHESS_CLIENT_ID'] =  os.getenv("LICHESS_CLIENT_ID")
-    app.config['LICHESS_CLIENT_SECRET'] = os.getenv("LICHESS_CLIENT_SECRET")
-    app.config['LICHESS_ACCESS_TOKEN_URL'] = 'https://oauth.lichess.org/oauth'
-    app.config['LICHESS_AUTHORIZE_URL'] = 'https://oauth.lichess.org/oauth/authorize'
-
-    oauth = OAuth(app)
-    oauth.register('lichess')
-
-    @app.route('/')
-    def login():
-        redirect_uri = url_for("authorize", _external=True)
-        """
-        If you need to append scopes to your requests, add the `scope=...` named argument
-        to the `.authorize_redirect()` method. For admissible values refer to https://lichess.org/api#section/Authentication.
-        Example with scopes for allowing the app to read the user's email address:
-        `return oauth.lichess.authorize_redirect(redirect_uri, scope="email:read")`
-        """
-        return oauth.lichess.authorize_redirect(redirect_uri)
-
-    @app.route('/authorize')
-    def authorize():
-        token = oauth.lichess.authorize_access_token()
-        bearer = token['access_token']
-        headers = {'Authorization': f'Bearer {bearer}'}
-        response = requests.get("https://lichess.org/api/account", headers=headers)
-        return jsonify(**response.json())
-
-    if __name__ == '__main__':
-        app.run()
-    '''
 
 whiteTime = 0.0
 
